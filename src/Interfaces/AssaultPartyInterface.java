2fe0a90c67dfef8d667ba915072f955b76bf09a5
Tiago Vilar <tiagovilar07@ua.pt>
2023-03-09 12:45:37 +0000

    Update -> AssaultParty And Museum Interfaces

---
+++ src/Interfaces/AssaultPartyInterface.java
@@ -0,0 +1,33 @@
+package Interfaces;
+
+/**
+ * AssaultParty is constituted by OrdinaryThiefs that are going to attack the museum.
+ * Assault party is shared by thieves
+ */
+
+public interface AssaultPartyInterface {
+
+    /**
+     * Called by the MasterThief to send the assaultParty to the museum
+     * After that call, AssaultParty can start crawling
+     */
+    
+    public void sendAssaultParty();
+
+    /**
+     * Called by the OrdinaryThief to crawlIn inside the museum
+     * @param thief -> thief id that is crawling In
+     * @return position of the thief after crawling In
+     */
+
+    public int crawlIn(int thief);
+
+    /**
+     * Called by the OrdinaryThief to crawlOut inside the museum
+     * @param thief -> thief id that is crawling Out
+     * @return position of the thief after crawling Out
+     */
+
+    public int crawlOut(int thief);
+
+}
---
+++ src/Interfaces/MuseumInterface.java
@@ -0,0 +1,24 @@
+package Interfaces;
+
+/**
+ * The Museum has rooms inside of it. That rooms have paintings that can be stolen by the OrdinaryThiefs of the AssaultParty
+ */
+
+public interface MuseumInterface{
+
+    /**
+     * Called by OrdinaryThief to get a canvas
+     * @return true if there are still paintings in the room, otherwise returns false
+     */
+
+    public boolean rollACanvas();
+
+    /**
+     * Called to awake the first member in the line of AssaultParty, by the last party member that rolled a canvas
+     * , so that the assaultParty can crawls Out
+     *  Synchronization Point -> between members of the AssaultParty
+     */
+
+    public void reverseDirection();
+
+}
