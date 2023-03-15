e8324e96875f2dc32c46af3a3b7a26f7e57cce8d
diogo-santos302 <diogoejsantos@ua.pt>
2023-03-15 19:00:28 +0000

    added Entry class, may be useful for logging

---
+++ src/Utils/Entry.java
@@ -0,0 +1,23 @@
+package src.Utils;
+
+public class Entry {
+    /**
+     * State of the Master Thief
+     */
+    private MasterThiefState masterThiefState;
+
+    /**
+     * Array with all Ordinary Thieves
+     */
+    private OrdinaryThief[] thievesInfo;
+
+    /**
+     * Array with the active Assault Parties
+     */
+    private AssaultParty[] assaultPartyInfo;
+
+    /**
+     * Array with all the museum's Rooms
+     */
+    private Room[] roomsInfo;
+}
