86f4f6a082e337fc528b6638dc244164737f1e41
Tiago Vilar <tiagovilar07@ua.pt>
2023-03-20 14:38:19 +0000

    .

--- src/SharedRegions/CollectionSite.java
+++ src/SharedRegions/CollectionSite.java
@@ -2,6 +2,14 @@ package src.SharedRegions;
 
 import src.Interfaces.CollectionSiteInterface;
 
-public CollectionSite{
-    
+public CollectionSite implements CollectionSiteInterface{
+
+     /**
+     * This is the first state change in the MasterThief life cycle it changes the MasterThief state to deciding what to do. 
+     * @throws Exception Exception
+     */
+    private void startOperations() throws Exception
+    {
+        
+    }
 }
