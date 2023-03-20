04dd6e33d91db480b9fb4c540aea240fd8df334d
Tiago Vilar <tiagovilar07@ua.pt>
2023-03-20 14:44:42 +0000

    .

--- src/SharedRegions/CollectionSite.java
+++ src/SharedRegions/CollectionSite.java
@@ -7,7 +7,7 @@ public CollectionSite implements CollectionSiteInterface{
      /**
      * This is the first state change in the MasterThief life cycle it changes the MasterThief state to deciding what to do. 
      */
-    private void startOperations(){
+    private synchronized void startOperations(){
         
     }
 }
