bbba0773a2a93db0ae08799c984fd5f76e344432
Tiago Vilar <tiagovilar07@ua.pt>
2023-03-20 14:56:48 +0000

    u

--- src/SharedRegions/CollectionSite.java
+++ src/SharedRegions/CollectionSite.java
@@ -4,26 +4,6 @@ import src.Interfaces.CollectionSiteInterface;
 
 public CollectionSite implements CollectionSiteInterface{
 
-     /**
-     * This is the first state change in the MasterThief life cycle it changes the MasterThief state to deciding what to do. 
-     */
-    private synchronized void startOperations(){
-        
-    }
-
-    public synchronized char appraiseSit(){
-
-    }
-
-    public synchronized void takeARest(){
-
-    }
-
-    public synchronized boolean collectACanvas(){
-
-    }
-
-    public synchronized void handACanvas(boolean canvas){
-
-    }
+    
+    
 }
---
+++ src/SharedRegions/ConcentrationSite.java
@@ -0,0 +1,29 @@
+package src.SharedRegions;
+
+import src.Interfaces.ConcentrarionSiteInterface;
+
+public ConcentrationSite implements ConcentrarionSiteInterface{
+
+     /**
+     * This is the first state change in the MasterThief life cycle it changes the MasterThief state to deciding what to do. 
+     */
+    public synchronized void startOperations(){
+        
+    }
+
+    public synchronized char appraiseSit(){
+
+    }
+
+    public synchronized void takeARest(){
+
+    }
+
+    public synchronized boolean collectACanvas(){
+
+    }
+
+    public synchronized void handACanvas(boolean canvas){
+
+    }
+}
