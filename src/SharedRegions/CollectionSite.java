0eba1ea6dfbab2013c9ce1d1e11c8d4ba6795ce6
Tiago Vilar <tiagovilar07@ua.pt>
2023-03-20 14:57:18 +0000

    Update CollectionSite.java

--- src/SharedRegions/CollectionSite.java
+++ src/SharedRegions/CollectionSite.java
@@ -4,6 +4,26 @@ import src.Interfaces.CollectionSiteInterface;
 
 public CollectionSite implements CollectionSiteInterface{
 
-    
-    
+     /**
+     * This is the first state change in the MasterThief life cycle it changes the MasterThief state to deciding what to do. 
+     */
+    public synchronized void startOperations(){
+        
+    }
+
+    public synchronized char appraiseSit(){
+
+    }
+
+    public synchronized void takeARest(){
+
+    }
+
+    public synchronized boolean collectACanvas(){
+
+    }
+
+    public synchronized void handACanvas(boolean canvas){
+
+    }
 }
